01d448d3461157926f33253b43303583dff2627f
wupeng5 <wupeng5@lenovo.com>
2022-04-05 00:34:04 +0800

    3

--- src/main/util/injector/config.js
+++ src/main/util/injector/config.js
@@ -5,6 +5,7 @@ import ModelFactory from "@model/factory";
 
 const IOCConfig = {
     init(){
+        //注册List model对象以及派生ListItem model子对象
         $rootInjector.register($MAP.TYPES.MODEL.LIST.TYPE, (...args)=>{
             let list = new ModelFactory().create($MAP.TYPES.MODEL.LIST.TYPE, ...args);
             list._data.forEach(x => {
@@ -13,11 +14,11 @@ const IOCConfig = {
             });
             return list;
         })
-
+        //创建ListItem model对象
         $rootInjector.register($MAP.TYPES.MODEL.LIST_ITEM.TYPE, (...args)=>{
             return new ModelFactory().create($MAP.TYPES.MODEL.LIST_ITEM.TYPE, ...args);
         })
-
+        //创建ListView对象以及派生ListItemView子视图
         $rootInjector.register($MAP.TYPES.VIEW.LIST.TYPE, (model)=>{
             let listView = new ViewFactory().create($MAP.TYPES.VIEW.LIST.TYPE, model);
             model._items.forEach(x=>{
@@ -26,11 +27,11 @@ const IOCConfig = {
             })
             return listView;
         })
-
+        //创建ListView子视图
         $rootInjector.register($MAP.TYPES.VIEW.LIST_ITEM.TYPE, (model)=>{
             return new ViewFactory().create($MAP.TYPES.VIEW.LIST_ITEM.TYPE, model);
         })
-
+        //创建InfoView视图
         $rootInjector.register($MAP.TYPES.VIEW.INFO.TYPE, (model)=>{
             return new ViewFactory().create($MAP.TYPES.VIEW.INFO.TYPE, model);
         })
