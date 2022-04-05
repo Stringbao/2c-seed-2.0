0422ad7b6542a2475f3d5cce0ee96596116c0f0a
wupeng5 <wupeng5@lenovo.com>
2022-04-05 21:55:53 +0800

    3

--- src/main/util/injector/injector.js
+++ src/main/util/injector/injector.js
@@ -1,13 +1,13 @@
 
 class Injector{
     constructor(){
-        this._iocMap = new Map();
+        this._injectorMap = new Map();
     }
     register(key, callback){
-        this._iocMap.set(key, callback);
+        this._injectorMap.set(key, callback);
     }
     use(key, ...args){
-        let res = this._iocMap.get(key);
+        let res = this._injectorMap.get(key);
         if(typeof res == "function"){
             return res(...args);
         }
